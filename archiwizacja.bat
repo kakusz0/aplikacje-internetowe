50bff26bdabfc104aaa676c7ffccaa77a549e6dd
kakusz <kakush1337@gmail.com>
2025-06-05 12:32:01 +0200

    uopdate

--- archiwizacja.bat
+++ archiwizacja.bat
@@ -1,1 +1,13 @@
-git archive
+@echo off
+REM Podaj swój numer albumu poniżej:
+set ALBUM=131358
+
+REM Ustaw nazwę archiwum
+set NAZWA=%ALBUM%_AI1_projekt.zip
+
+REM Tworzymy archiwum git (pomija vendor i node_modules automatycznie)
+git archive --format=zip -o "%NAZWA%" HEAD
+
+REM Informacja dla użytkownika
+echo Gotowe! Archiwum zapisane jako %NAZWA%
+pause
